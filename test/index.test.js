fecb1fc05b01011be4bada2eda265209203187d4
linchen <linchen@boqii.com>
2017-03-29 18:24:26 +0800

    upgrage APPID for test

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,16 @@
+import fs from 'fs'
 import path from 'path'
 import assert from 'assert'
 import Alipay from '../src/index'
 
-const resolve = filename => {
-  return path.resolve(__dirname, filename)
+const read = filename => {
+  return fs.readFileSync(path.resolve(__dirname, filename))
 }
 
 const options = {
-  app_id: '2016080300160334',
-  notify_url: 'http://test.tangide.com/alipap-test.php',
-  appPrivKeyFile: resolve('./keys/app_priv_key.pem'),
-  alipayPubKeyFile: resolve('./keys/alipay_public_key.pem')
+  app_id: '2016080100137766',
+  appPrivKeyFile: read('./keys/app_priv_key.pem'),
+  alipayPubKeyFile: read('./keys/alipay_public_key.pem')
 }
 
 const service = new Alipay(options)
@@ -31,25 +31,19 @@ describe('ALIPAY unit test', function () {
 
   it('should allow query order', () => {
     const outTradeNo = '1232423'
-    return service.queryOrder(outTradeNo)
+    return service.queryOrder({ out_trade_no: outTradeNo })
     .then(result => {
       assert(result.code == -1, result.message)
-      assert(result.data.code == '40004')
-      assert(result.data.sub_msg === '交易不存在')
-    })
-  })
-  it ('should reject makeNotifyResponse', () => {
-    return service.makeNotifyResponse({})
-    .then(result => {
-      console.dir(result)
-      assert(result.code == -1, result.message)
+      assert(result.message == 'error', result.message)
+      assert(result.data['alipay_trade_query_response'].code === '40004')
+      assert(result.data['alipay_trade_query_response'].sub_msg === '交易不存在')
     })
   })
 
-  // it ('should reject makeNotifyResponse', () => {
-  //   return service.makeNotifyResponse()
-  //   .then(result => {
-  //     assert(result.code == -1, result.message)
-  //   })
-  // })
+  it ('should reject makeNotifyResponse', () => {
+    return service.makeNotifyResponse({})
+    .then(result => {
+      assert(result.code == -1, result.message)
+    })
+  })
 })
